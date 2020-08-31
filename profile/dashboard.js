d1f38cb66d7202452146639a55171ac8fede77ac
quamepossible <appiahkwame274@gmail.com>
2020-08-31 13:21:50 +0000

    Just a simple change

--- profile/dashboard.js
+++ profile/dashboard.js
@@ -9,7 +9,7 @@ function validate(){
 
     for(var i = 3; i < inpLen; i++){
 
-        if((i >= 6 && i <= 8) || (i == 11)){
+        if((i >= 5 && i <= 7) || (i == 10)){
             continue;
         }
         if(inputs[i].value.length == 0){
