e9be43edb48ba2076fbb2a98eb80b607691b358e
quamepossible <appiahkwame274@gmail.com>
2020-09-09 16:45:51 +0000

    rating stars are now responsive across all devices

--- dash/dasheader.php
+++ dash/dasheader.php
@@ -11,7 +11,7 @@
         <script src="user.js" defer></script>
         <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule="" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
-        <meta name="theme-color" content="#007d9c">
+        <meta name="theme-color" content="black">
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <title><?php echo ucfirst($_GET['name'])?></title>
     </head>
