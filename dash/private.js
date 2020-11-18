fb529ea9a7c50ad2f41b77012e83434954ebe23b
quamepossible <appiahkwame274@gmail.com>
2020-11-18 22:10:39 +0000

    swal icons modified

--- dash/private.js
+++ dash/private.js
@@ -140,7 +140,7 @@ $(document).ready(function(){
                 else if(data == 'empty'){
                     swal.fire({
                         title: 'upload a picture',
-                        icon: 'error'
+                        icon: 'info'
                     })
                 }
 
